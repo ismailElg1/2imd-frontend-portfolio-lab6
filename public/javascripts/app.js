b0595050194ce5fc8528de9e040ccf64df209eb0
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-24 12:20:45 +0100

    feat: start primus connection and detect when someone made connection

---
+++ public/javascripts/app.js
@@ -0,0 +1,7 @@
+primus = Primus.connect('/', {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , min: 500 // Number: The minimum delay before we try reconnect.
+      , retries: 10 // Number: How many times we should try to reconnect.
+    }
+  });
