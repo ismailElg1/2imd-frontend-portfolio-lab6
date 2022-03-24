9943718afb2d6306b6ce13456a8d56accf6440ab
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-03-24 12:33:52 +0100

    feat: update score button now sends primus json file

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -6,3 +6,35 @@ primus = Primus.connect('/', {
     }
   });
 
+  document.querySelector('.updateButton').addEventListener('click', e =>{
+      let team = document.querySelector('.teamDropdown').value;
+      let score = document.querySelector('.scoreInput').value;
+     
+      switch(team){
+        case "team1":  primus.write({
+            team : "team1",
+            score: score
+          });
+          break;
+          case "team2":  primus.write({
+            team : "team2",
+            score: score
+          });
+          break;
+          case "team3":  primus.write({
+            team : "team1",
+            score: score
+          });
+          break;
+          case "team4":  primus.write({
+            team : "team4",
+            score: score
+          });
+          case "team5":  primus.write({
+            team : "team5",
+            score: score
+          });
+          break;
+          
+      }
+});
